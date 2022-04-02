a0888c4d5fb69a77d8e91ec0a908d0c942c91ba6
rustam-cloud <gasymovrustam86@Yandex.com>
2022-04-02 16:24:00 +0300

    fix: fix errors related to rendering elements after switching map view mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as Cesium from "cesium";
 
 import styles from "./App.module.css";
@@ -11,40 +11,32 @@ Cesium.Ion.defaultAccessToken =
 export const App = () => {
   const viewerElementRef = useRef<any>(null);
   const [isVisible, setIsVisible] = useState(false);
-  const [data, setData] = useState<any>([]);
+  const fakeDataRef = useRef<any>(null);
 
   useEffect(() => {
     if (viewerElementRef.current) {
       viewerElementRef.current = new Cesium.Viewer(viewerElementRef?.current, {
-        requestRenderMode: true,
         maximumRenderTimeChange: Infinity,
         infoBox: false,
         selectionIndicator: false,
         shouldAnimate: true,
-        // targetFrameRate: 30
-        // terrainProvider: Cesium.createWorldTerrain(),
       });
-      // viewerElementRef.current.scene.debugShowFramesPerSecond = true;
-      // viewerElementRef.current.scene.globe.enableLighting = false;
-      // viewerElementRef.current.scene.globe.depthTestAgainstTerrain = false;
     }
-  }, [viewerElementRef]);
+  }, []);
 
   useEffect(() => {
     (async () => {
       if (viewerElementRef && isVisible) {
-        const fakeData = await initFakeData();
-        createEntities(viewerElementRef, fakeData);
-        // setData(fakeData);
+        fakeDataRef.current = await initFakeData();
+        createEntities(viewerElementRef, fakeDataRef.current);
       }
 
       if (!isVisible) {
+        fakeDataRef.current = null;
         viewerElementRef.current.entities.removeAll();
       }
-
-      // console.log("current viewer", viewerElementRef.current.entities);
     })();
-  }, [viewerElementRef, isVisible]);
+  }, [viewerElementRef, isVisible, fakeDataRef]);
 
   return (
     <div ref={viewerElementRef}>
