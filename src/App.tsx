4c165b8ea5831c54ba94b148bc0169e5811aacff
rustam-cloud <gasymovrustam86@Yandex.com>
2022-04-02 15:03:51 +0300

    fix: bug render

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ export const App = () => {
 
       // console.log("current viewer", viewerElementRef.current.entities);
     })();
-  }, [viewerElementRef, isVisible, data]);
+  }, [viewerElementRef, isVisible]);
 
   return (
     <div ref={viewerElementRef}>
